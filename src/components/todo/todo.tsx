1a883ccd5438fe5691fae1c2e7f6713859a99414
Jahanzaib Tayyab <jahanzaib.tayyab@devigital.com>
2023-01-18 21:36:09 +0500

    chakra ui integration with theme

---
+++ src/components/todo/todo.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import { Input, Button } from "@chakra-ui/react";
+
+const Todo = () => {
+  return (
+    <div>
+      Todo
+      <Input />
+      <Button>Click me</Button>
+    </div>
+  );
+};
+export default Todo;
